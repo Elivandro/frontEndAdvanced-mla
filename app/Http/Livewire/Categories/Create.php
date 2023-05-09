b2d8b2f49389ada35f9e3eb939df1f50de8cea8f
Elivandro <Elivandro@hotmail.com.br>
2023-05-08 20:49:16 -0400

    livewire component and view created

---
+++ app/Http/Livewire/Categories/Create.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire\Categories;
+
+use Livewire\Component;
+use App\Models\Category;
+
+class Create extends Component
+{
+    public Category $category;
+
+    protected $rules = [
+        'category.name' => 'required|string|min:4|max:255|unique:categories,name'
+    ];
+
+    protected $messages = [
+        'category.name.required' => 'Campo categoria obrigarotio',
+        'category.name.string' => 'o campo precisa ser do tipo texto',
+        'category.name.min' => 'quantidade minima 4 caracteres',
+        'category.name.max' => 'quantidade maxima 255 caracteres',
+        'category.name.unique' => 'categoria já existente'
+    ];
+
+    public function mount()
+    {
+        $this->category = new Category();
+    }
+
+    public function updated($property)
+    {
+        $this->validateOnly($property);
+
+    }
+
+    public function render()
+    {
+        return view('livewire.categories.create');
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->category->save();
+    }
+}
